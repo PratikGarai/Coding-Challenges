0867c4c1689e1ae2335731aab13d3d762c4e87ba
PratikGarai <pratikgarai0208@gmail.com>
2020-08-15 13:41:49 +0530

    EllysThreePrime Completed

--- topcoder/EllysThreePrimes.py
+++ topcoder/EllysThreePrimes.py
@@ -44,7 +44,7 @@ def get_third(first, second, sums_list, digits):
     p_multiplier = 1  #new
     for i in range(digits) :
         digit = sums_list[i]-(int((first%c_multiplier)/p_multiplier)+int((second%c_multiplier)/p_multiplier))
-        if digit<0:
+        if digit<0 or digit>9:
             return -1
         s += digit*p_multiplier
         p_multiplier, c_multiplier = c_multiplier, c_multiplier*10
@@ -66,27 +66,27 @@ def generate_triplets(start, end, primes_list, sums_list):
                 low = mid+1
         return False
 
-    # test code 
-    if(20533 in l and 44927 in l and 87179 in l):
-        print("Prime list check")
-    if(check_presence(20533)):
-        print("Binary search check 1")
-    if(check_presence(44927)):
-        print("Binary search check 2")
-    if(check_presence(87179)):
-        print("Binary search check 3")
-    if(get_third(20533,44927,sums_list,5)==87179):
-        print("Generation correct ",get_third(20533,44927,sums_list,5))
-    else :
-        print("Generation incorrect ",get_third(20533,44927,sums_list,5))
+    # test code
+    # if(20533 in l and 44927 in l and 87179 in l):
+    #     print("Prime list check")
+    # if(check_presence(20533)):
+    #     print("Binary search check 1")
+    # if(check_presence(44927)):
+    #     print("Binary search check 2")
+    # if(check_presence(87179)):
+    #     print("Binary search check 3")
+    # if(get_third(20533,44927,sums_list,5)==87179):
+    #     print("Generation correct ",get_third(20533,44927,sums_list,5))
+    # else :
+    #     print("Generation incorrect ",get_third(20533,44927,sums_list,5))
 
-    # for i in range(le):
-    #     for j in range(i+1,le):
-    #         third = get_third(l[i], l[j], sums_list,5)
-    #         if(third==-1):
-    #             continue
-    #         if check_presence(third):
-    #             return [l[i], l[j], third]
+    for i in range(le):
+        for j in range(i+1,le):
+            third = get_third(l[i], l[j], sums_list,5)
+            if(third==-1):
+                continue
+            if check_presence(third):
+                return [l[i], l[j], third]
     return []
 
 def get3(a):
@@ -104,3 +104,17 @@ def main():
 
 if __name__=='__main__':
     main()
+
+
+# TEST CASES
+#-------------------------------
+# Case 1 : 
+# 19 12 15 11 14
+# 10007   42643   99989
+# Time :  0.06131744384765625
+#-------------------------------
+# Case 2 : (worst case)
+# 22 19 3 8 23
+
+# Time :  22.881287574768066
+#-------------------------------
