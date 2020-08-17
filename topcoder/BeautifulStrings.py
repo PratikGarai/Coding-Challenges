e91a777e9037b7e8a5cb0399056c1a4aaa757826
PratikGarai <pratikgarai0208@gmail.com>
2020-08-17 19:01:34 +0530

    Made the BeautifulStrings code

---
+++ topcoder/BeautifulStrings.py
@@ -0,0 +1,38 @@
+import time
+
+def compute(ca,cb,ma,mb):
+    if ma==0 or ca==0:
+        return mb
+    if mb==0 or cb==0:
+        return ma
+    if ca==cb :
+        return 2*ca
+    min_groups = 0
+    max_groups = 0
+    max_count = 0
+    if ca<cb :
+        min_groups = ca
+        max_groups = ca+2
+        max_limit = mb
+        max_count = ma
+    else cb<ca :
+        min_groups = cb
+        max_groups = cb+2
+        max_limit = ma
+        max_count = ma
+    if max_groups*max_limit>=max_count:
+        return min_groups+le_final+max_count
+    else :
+        return min_groups+max_groups*max_count
+
+
+def main():
+    countA = int(input())
+    countB = int(input())
+    maxA = int(input())
+    maxB = int(input())
+
+    print(compute(countA, countB, maxA, maxB))
+
+if __name__ == '__main__':
+    main()
