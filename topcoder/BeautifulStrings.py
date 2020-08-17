5f6e521dad9610552b118b4855c161edddd5b8a4
PratikGarai <pratikgarai0208@gmail.com>
2020-08-17 19:16:20 +0530

    Fully tested and verified code

--- topcoder/BeautifulStrings.py
+++ topcoder/BeautifulStrings.py
@@ -1,4 +1,6 @@
 def compute(ca,cb,ma,mb):
+    if (ma==0 and mb==0) or (ca==0 and cb==0) :
+        return 0
     if ma==0 or ca==0:
         return mb
     if mb==0 or cb==0:
@@ -7,18 +9,18 @@ def compute(ca,cb,ma,mb):
         return 2*ca
     if ca<cb :
         min_groups = ca
-        max_groups = ca+2
+        max_groups = ca+1
         max_limit = mb
-        max_count = ma
+        max_count = cb
     else :
         min_groups = cb
-        max_groups = cb+2
+        max_groups = cb+1
         max_limit = ma
-        max_count = ma
+        max_count = ca
     if max_groups*max_limit>=max_count:
-        return min_groups+le_final+max_count
+        return min_groups+max_count
     else :
-        return min_groups+max_groups*max_count
+        return min_groups+max_groups*max_limit
 
 
 def main():
