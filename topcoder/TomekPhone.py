12ed74f182040424c606b895794b56c596104543
PratikGarai <pratikgarai0208@gmail.com>
2020-08-18 18:59:18 +0530

    Completed and tested TomekPhone

--- topcoder/TomekPhone.py
+++ topcoder/TomekPhone.py
@@ -2,9 +2,19 @@ def get_keytaps(target_set, key_capacity):
     le = len(target_set)
     if le>sum(key_capacity):
         return -1
+    buttons = len(key_capacity)
     target_set = sorted(target_set, reverse=True)
     key_capacity = sorted(key_capacity)
-    return 0
+    multipliers  = [ 1 for i in range(buttons) ]
+    pointer = 0
+    keytaps = 0
+    for i in range(le):
+        keytaps += multipliers[pointer]*target_set[i]
+        multipliers[pointer] += 1
+        pointer += 1
+        if pointer==buttons :
+            pointer = 0
+    return keytaps
 
 def main():
     target_set = list(map(int,input().split()))
