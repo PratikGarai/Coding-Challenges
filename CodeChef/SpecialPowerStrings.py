0ed9356fa94de1bcb4f33d7d3a411e96de10eb9e
PratikGarai <pratikgarai0208@gmail.com>
2020-08-19 18:14:57 +0530

    Added full code of SpecialPowerString

--- CodeChef/SpecialPowerStrings.py
+++ CodeChef/SpecialPowerStrings.py
@@ -1,3 +1,5 @@
+import time 
+
 def make_dict(string):
     d  = {}
     for i in range(97,123):
@@ -13,15 +15,32 @@ def subs_dict(d1, d2):
             s += chr(i)
     return s
 
-def is_covered_by(main, sub):
-    pass
+def get_set(string):
+    s = set()
+    for i in string : 
+        s.add(i)
+    return s
 
 def compute(s1,s2,hx,qm,ex):
     d1 = make_dict(s1)
     d2 = make_dict(s2)
     diff = subs_dict(s1,s2)
-    if len(diff)==0:
+    l = len(diff)
+    if l==0:
         return 0
+    ds = get_set(diff)
+    hs = get_set(hx)
+    qe = get_set(qm)
+    es = get_set(ex)
+
+    if ds-hs=={} or ds-qs=={} or ds-es=={}:
+        return 1
+    if ds-(hs|qs)=={} or ds-(qs|es) or ds-(hs|es):
+        return 2
+    if ds-(hs|qs|es):
+        return 3
+    else: 
+        return -1
 
 def main():
     s1 = input()
@@ -29,7 +48,15 @@ def main():
     hx = input()
     qm = input()
     ex = input()
-    print(compute(s1,s2,hx,qm,ex))
+    a = time.time()
+    res = compute(s1,s2,hx,qm,ex)
+    if res==-1 :
+        print('Not possible')
+    else :
+        print(res)
+    b = time.time()
+    print()
+    print('Computation time : ',(b-a))
 
 if __name__=='__main__':
     main()
