e13f429f65cc07889f31afe1cbdb9b52fcd901e3
ritikasarkar18 <ritikasarkar438@gmail.com>
2020-08-21 21:37:57 +0530

    Lisa done

--- Hackerrank/Lisa's Workbook avg(Medium).py
+++ Hackerrank/Lisa's Workbook avg(Medium).py
@@ -1,8 +1,22 @@
-nk=input().split()
-n=int(nk[0])
-k=int(nk[1])
-arr=list(map(int,input().split()))
-prob=[]; c=0
+N, K = map(int, input().split())
+T = list(map(int, input().split()))
+
+cnt = 0 # special problems
+i = 0   # chapter number
+j = 1   # page number
+m = 1   # problem number
+
+while i < N:
+    if m <= j and j <= min(m + K - 1, T[i]):
+        cnt += 1
+    j += 1
+    m += K
+    if m > T[i]: # next chapter
+        i += 1
+        m = 1
+print(cnt)
+    
+'''
 for i in range(n):
     if arr[i]>k:
         copy=k
@@ -16,11 +30,19 @@ for i in range(n):
 num=[i+1 for i in range(len(prob))]
 book=dict(zip(num,prob))
 
-num=[i+1 for i in range(len(arr))]
+num1=[i+1 for i in range(len(arr))]
 count=dict(zip(num1,arr))
 
-'''for i in range(1,len(count)+1):
+j=(1,0)
+for i in range(1,len(book)+1):
     
+    if (i>1)
+        j=(book[i-1][1],book[i-1][0])
+    
+    if (book[i][1]==j):
+        if i in range(1,book[i][0]+1):
+            c+=1
+        if i in range(1,book[i][0])
 
-    
-print(c)'''
+print(c)
+'''
