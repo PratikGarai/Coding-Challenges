3efba5db7e8f4474d7ebab1365f587e2ef55711e
ritikasarkar18 <ritikasarkar438@gmail.com>
2020-08-21 23:53:01 +0530

    Debugged Append and Delete.py

--- Hackerrank/Append and Delete (Easy).py
+++ Hackerrank/Append and Delete (Easy).py
@@ -5,6 +5,11 @@ k=int(input())
 l=min(len(s),len(t))
 m=0
 i=0
+
+if (len(s)+len(t)<k):
+    print("Yes")
+    exit()
+
 while(i<l):
     if s[i]!=t[i]:
         break 
@@ -13,7 +18,7 @@ while(i<l):
 p=len(s)-i
 q=len(t)-i
 
-if (p+q<=k):
+if (p+q<=k and (k-p-q)%2==0):
     print("Yes")
 else:
     print("No")
