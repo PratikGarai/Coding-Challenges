0996a5320c07a22b67247922c7350109007cd176
PratikGarai <pratikgarai0208@gmail.com>
2020-08-22 11:39:23 +0530

    Basic structure

---
+++ topcoder/NextOlympics.py
@@ -0,0 +1,15 @@
+def get_days(d):
+    pass
+
+def get_diff(b,a):
+    pass
+
+def main():
+    a = input()
+    b = input()
+    ad = get_days(a)
+    bd = get_days(b)
+    print(get_diff(b,a))
+
+if __name__ == '__main__':
+    main()
