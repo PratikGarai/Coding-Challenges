c0459980dcec12cb2cc01614f2a1b6c28ae1518a
PratikGarai <pratikgarai0208@gmail.com>
2020-08-24 17:29:08 +0530

    Solution cleaned

--- Hackerrank/MaxMin.py
+++ Hackerrank/MaxMin.py
@@ -1,12 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the maxMin function below.
 def maxMin(k, arr):
     arr = sorted(arr)
     l = len(arr)
@@ -22,21 +13,15 @@ def maxMin(k, arr):
             m = arr[i+k-1]-arr[i]
     return m
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+def main():
     n = int(input())
-
     k = int(input())
-
     arr = []
-
-    for _ in range(n):
-        arr_item = int(input())
-        arr.append(arr_item)
-
+    for i in range(n):
+        a = int(input())
+        arr.append(a)
     result = maxMin(k, arr)
+    print(result)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+if __name__ == '__main__':
+    main()
