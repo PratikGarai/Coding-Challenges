0da54e44986f5e8f3a957a197002e44cc04d1430
PratikGarai <pratikgarai0208@gmail.com>
2020-08-27 10:47:32 +0530

    Some stuff done

--- topcoder/KingFactorization.py
+++ topcoder/KingFactorization.py
@@ -1,5 +1,16 @@
+def get_mid(a,b):
+    return 0
+
 def compute(n,l):
-    pass
+    le = len(l)
+    lst = []
+    for i in range(0, le-1):
+        lst.append(l[i])
+        n = int(n/l[i])
+        g = get_mid(l[i],l[i+1]))
+        n = int(n/g)
+        lst.append(g)
+    return lst
 
 def  main():
     n = int(input())
