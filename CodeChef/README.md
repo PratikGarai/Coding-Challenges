102ceda424675ba49c7068a72f1ca8ce28afe1b6
PratikGarai <pratikgarai0208@gmail.com>
2020-08-30 15:58:13 +0530

    cleared thing

--- CodeChef/README.md
+++ CodeChef/README.md
@@ -12,6 +12,6 @@
 <td>2.</td><td>Cherry and Squares</td><td>https://www.codechef.com/problems/CENS20B</td>
 </tr>
 <tr>
-<td>3.</td><td>Subsegment Sum</td><td>https://www.codechef.com/problems/SEGSM</td>
+<td>3.</td><td>Subsegment Sum</td><td>https://www.codechef.com/problems/SEGSM (incomplete)</td>
 </tr>
 </table>
--- CodeChef/SubsegmentSum.py
+++ CodeChef/SubsegmentSum.py
@@ -1,3 +1,5 @@
+def min_counter(a,n):
+
 def main():
     n = int(input())
     a = list(map(int, input().split()))
@@ -5,19 +7,7 @@ def main():
     for i in range(t):
         s = list(map(int, input().split()))
         if s==[2]:
-            gsum = 0
-            s_a = sorted(a)
-            count = 0
-            for i in range(n):
-                count += 1
-                try :
-                    if s_a[i+1]>s_a[i]+1:
-                        gsum += count
-                        count = 0
-                except :
-                    break
-            gsum += count
-            print("Result : ",gsum)
+            result = min_counter(sorted(a), n)
         else :
             a[s[1]-1] = s[2]
 
