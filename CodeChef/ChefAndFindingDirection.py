df4406413925a423c44b8e74ebdcabf7a6dde124
PratikGarai <pratikgarai0208@gmail.com>
2020-08-31 10:22:29 +0530

    ALgo completely changed, still no results

--- CodeChef/ChefAndFindingDirection.py
+++ CodeChef/ChefAndFindingDirection.py
@@ -4,22 +4,25 @@ dy = {'L':0,'R':0,'U':1,'D':-1}
 def compute(a, target,n):
     if target==1:
         return True
-    visited = [0 for i in range(target)]
-    v = [([0,0],0)]
-    max_val = 0
-    while v!=[]:
-        lv = v.pop()
-        val = lv[1]
-        if val>max_val:
-            max_val = val
-        p = lv[0]
-        visited[p[0]*n+p[1]]=1
-        for i in a[p[0]][p[1]]:
-            if visited[(p[0]+dy[i])*n+(p[1]+dx[i])]==0:
-                v.append(([p[0]+dy[i], p[1]+dx[i]],val+1))
-    print(max_val)
-    if max_val==target:
-        return True
+    visited = [[0 for i in range(n)] for j in range(n)]
+    def traverse(pos , value):
+        if value==target-1:
+            if pos==[0,0]:
+                return True
+            else:
+                return False
+        if visited[pos[0]][pos[1]]==1:
+            return False
+        visited[pos[0]][pos[1]]=1
+        l = []
+        for i in a[pos[0]][pos[1]]:
+            l.append(traverse((pos[0]+dy[i], pos[1]+dx[i]), value+1))
+        if any(l):
+            return True
+        else :
+            return False
+    result = traverse([0,0], 0)
+    print(result)
 
 def main():
     t = int(input())
