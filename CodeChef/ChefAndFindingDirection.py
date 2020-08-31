a060af62288680c4f637e0d29de3ad8453afc6bc
PratikGarai <pratikgarai0208@gmail.com>
2020-08-31 10:39:08 +0530

    Validity proved, need to conserve path now

--- CodeChef/ChefAndFindingDirection.py
+++ CodeChef/ChefAndFindingDirection.py
@@ -1,27 +1,29 @@
 dx = {'L':-1,'R':1,'U':0,'D':0}
-dy = {'L':0,'R':0,'U':1,'D':-1}
+dy = {'L':0,'R':0,'U':-1,'D':1}
 
 def compute(a, target,n):
     if target==1:
         return True
     visited = [[0 for i in range(n)] for j in range(n)]
-    def traverse(pos , value):
-        if value==target-1:
-            if pos==[0,0]:
+    def traverse(y,x,value):
+        # print(y,x)
+        if value==target:
+            if x==0 and y==0:
                 return True
             else:
                 return False
-        if visited[pos[0]][pos[1]]==1:
+        if visited[y][x]==1:
             return False
-        visited[pos[0]][pos[1]]=1
+        visited[y][x] = 1
         l = []
-        for i in a[pos[0]][pos[1]]:
-            l.append(traverse((pos[0]+dy[i], pos[1]+dx[i]), value+1))
+        for i in a[y][x]:
+            l.append(traverse(y+dy[i], x+dx[i], value+1))
+        visited[y][x] = 0
         if any(l):
             return True
         else :
             return False
-    result = traverse([0,0], 0)
+    result = traverse(0,0, 0)
     print(result)
 
 def main():
