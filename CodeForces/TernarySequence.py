b509a08f2ac9971fe6ceb56e4794797fd9cb08ef
PratikGarai <pratikgarai0208@gmail.com>
2020-09-03 18:04:16 +0530

    Debugged some stuff

--- CodeForces/TernarySequence.py
+++ CodeForces/TernarySequence.py
@@ -1,7 +1,6 @@
 def process(a,b,c,d,e,f):
     s = 0
 
-    #handling 2 using 1
     if c>e:
         s = 2*e
         c -= e
@@ -10,27 +9,25 @@ def process(a,b,c,d,e,f):
         s = 2*c
         c = 0
         e -= c
-    #handling 2 using 2
     if c>f :
         return s
     else :
         c = 0
         f -= c
 
-   #handling 1
-   if b<=d+e :
-       return s
-   else :
-       s -= 2*f
-       return s
+    if b<=d+e:
+        return s
+    else :
+        s -= 2*f
+        return s
 
 def main():
-    n = input()
+    n = int(input())
     results = []
     for i in range(n):
         f0, f1, f2 = list(map(int, input().split()))
         s0, s1, s2 = list(map(int, input().split()))
-        results.append(process(f0, f1, f2, s1, s2, s3))
+        results.append(process(f0, f1, f2, s0, s1, s2))
 
     for i in results:
         print(i)
