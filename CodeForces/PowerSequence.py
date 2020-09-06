c5d1d3cc1355add8426cf93e69e33820da0b0280
PratikGarai <pratikgarai0208@gmail.com>
2020-09-06 18:33:50 +0530

    No solution for TLE yet

--- CodeForces/PowerSequence.py
+++ CodeForces/PowerSequence.py
@@ -10,9 +10,11 @@ def main():
     arr = sorted(arr)
     max_cost = get_cost(arr, 1)
     high_limit = int(max_cost**(1/(n-1)))+1
+
+    print(high_limit)
     
     min_cost = max_cost
-    for i in range(2, 2*high_limit+1):
+    for i in range(2, high_limit+1):
         min_cost = min(min_cost,get_cost(arr, i))
 
     print(min_cost)
