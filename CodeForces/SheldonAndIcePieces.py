f749a0a7dee34d7a296ce8a99264f90b5bb3441e
PratikGarai <pratikgarai0208@gmail.com>
2020-09-09 05:07:36 +0530

    More computations added

--- CodeForces/SheldonAndIcePieces.py
+++ CodeForces/SheldonAndIcePieces.py
@@ -4,28 +4,34 @@ def factorial(n):
         p *= i
     return p
 
-def get_permutatuions(a,b,c):
-    return int(factorial(b+c)/(factorial(b+c-a)*factorial(b)*factorial(c)))
+def get_permutatuions(a,b,c,d):
+    if (a+b)>(c+d):
+        return 0
+    return int(factorial(c+d)/(factorial(c+d-a-b)*factorial(c)*factorial(d)))
 
-def get_dict(s):
-    n2,n5,n6,n9 = 0,0,0,0
+def is_valid(n,m):
+    for i in range(10):
+        if i==2 or i==5 or i==6 or i==9:
+            continue
+        if n[i]>m[i]:
+            return False
+    return True
+
+def get_list(s):
+    l = [0 for i in range(10)]
     for i in s:
-        if i=='2':
-            n2 += 1
-        elif i=='5':
-            n5 += 1
-        elif i=='6':
-            n6 += 1
-        elif i=='9':
-            n9 += 1
-    return (n2,n5,n6,n9)
+        l[ord(i)-48] += 1
+    return l
 
 def main():
     n = input()
     m = input()
-    nd = get_dict(n)
-    md = get_dict(m)
-    print(get_permutatuions(nd[0]+nd[1], md[0], md[1])*get_permutatuions(nd[2]+nd[3], md[2], md[3]))
+    nd = get_list(n)
+    md = get_list(m)
+    if is_valid(nd, md):
+        print(get_permutatuions(nd[2],nd[5],md[2],md[5])*get_permutatuions(nd[6],nd[9],md[6],md[9]))
+    else:
+        print(0)
 
 if __name__=='__main__':
     main()
