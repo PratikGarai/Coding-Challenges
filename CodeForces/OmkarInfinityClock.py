033d232a4488be5caedb8127ac2a646873e2681b
PratikGarai <pratikgarai0208@gmail.com>
2020-09-17 16:37:47 +0530

    new solution added

--- CodeForces/OmkarInfinityClock.py
+++ CodeForces/OmkarInfinityClock.py
@@ -2,6 +2,34 @@ def get_result():
     n, k = list(map(int, input().split()))
     l = list(map(int, input().split()))
 
+    if k==0 :
+        return k
+    mx = max(l)
+    mi = min(l)
+    for i in range(n):
+        l[i] = mx-l[i]
+    #all negatives turn to positive
+    if k==1:
+        return l
+
+    mx = max(l)
+    for i in range(n):
+        l[i] = mx-l[i]
+    #the first 0 appears
+    if k==2:
+        return l
+
+    mx =  max(l)
+    if mi<0:
+        k -= 1
+    if k%2==1:
+        for i in range(n):
+            if l[i]==0:
+                l[i] = mx
+            elif l[i]==mx:
+                l[i] = 0
+            else:
+                l[i] = mx-l[i]
     return l
 
 def main():
