35f5b0edf0ffbb34961ac7e6743b95845d9857fc
PratikGarai <pratikgarai0208@gmail.com>
2020-09-18 13:54:30 +0530

    Done!

--- CodeForces/BuyingTorches.py
+++ CodeForces/BuyingTorches.py
@@ -1,17 +1,12 @@
+import math 
+
 def get_result():
     x,y,k = list(map(int,input().split()))
 
-    coals = k
-    sticks_for_coals = y*k
-    sticks_needed = k
-    total_sticks_needed = sticks_for_coals+sticks_needed
+    t = (y+1)*k-1+x-2
+    trades = t//(x-1) + k
 
-    trades_for_sticks = (total_sticks_needed-1 )//(x-1)
-    if ((total_sticks_needed-1)/(x-1))%1 != 0 :
-        trades_for_sticks += 1
-    trades_for_coals = k
-
-    return trades_for_sticks+trades_for_coals
+    return int(trades)
 
 def main():
     t = int(input())
