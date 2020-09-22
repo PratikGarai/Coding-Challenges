12cba6cf6af1b343ccdceb5c21d58e4e9e67a58b
PratikGarai <pratikgarai0208@gmail.com>
2020-09-22 17:57:55 +0530

    Another wrong code

--- CodeForces/SageBirthday2.py
+++ CodeForces/SageBirthday2.py
@@ -4,48 +4,31 @@ def main():
     l = sorted(l)
     mx = l[-1]
 
-    p = [0 for i in range(n)]
+    occupied = [0 for i in range(n)]
     
     l_pointer = 0
-    h_pointer1 = l_pointer + 1
-    h_pointer2 = l_pointer + 2
-    
-    while(l[l_pointer] != mx or l_pointer<n-2):
-        
-        #loop to find h_pointer1
-        while(l[h_pointer1]<=l[l_pointer] and l[h_pointer1]==1):
-            h_pointer1 += 1
-            if h_pointer1==n-1:
-                break
-        if h_pointer1==n-1:
-            break
-        p[h_pointer1] = 1
+    h_pointer = l_pointer + 1
 
-        h_pointer2 = h_pointer1+1 
-        #loop to find h_pointer2
-        while(l[h_pointer2]<=l[l_pointer]):
-            h_pointer2 += 1
-            if h_pointer2==n:
-                break
-        if h_pointer2==n:
-            break
-        p[h_pointer2] = 1
-        
-        print(l[h_pointer1],l[l_pointer],l[h_pointer2],sep=' ', end=' ')
+    while(h_pointer<n-1 and l_pointer<n-2):
 
-        l_pointer += 1
-        #loop to find new l_pointer
-        while(p[l_pointer]==1):
+        occupied[l_pointer] = 1
+
+        while(l[h_pointer]<=l[l_pointer] and h_pointer<n-1):
+            h_pointer += 1
+
+        print(l[h_pointer],l[l_pointer],l[h_pointer+1], sep=' ', end=' ')
+        occupied[h_pointer] = 1
+        occupied[h_pointer+1] = 1
+        h_pointer += 1
+
+        while(l[l_pointer]!=0 and l_pointer<n-2):
             l_pointer += 1
-            if l_pointer==n-2:
-                break
-        if l_pointer==n-2:
-            break
+        if l_pointer>=h_pointer:
+            h_pointer = l_pointer+1
 
     for i in range(n):
-        if p[i]!=1:
+        if occupied[i]==0:
             print(l[i], end=' ')
-
     print()
 
 if __name__=='__main__':
