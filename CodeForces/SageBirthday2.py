baa581fd85d1ce440fd9597f8ac41d170c8fdd63
PratikGarai <pratikgarai0208@gmail.com>
2020-09-24 01:10:40 +0530

    Added some code

--- CodeForces/SageBirthday2.py
+++ CodeForces/SageBirthday2.py
@@ -6,6 +6,19 @@ def main():
 
     occupied = [0 for i in range(n)]
     
+    low = 0
+    high = 1
+    num = 0
+
+    while(l[high]==l[low] and high<n):
+        high += 1
+
+    occupied[low] = 1
+    occupied[high] = 1
+
+    while(low<n-1 and high<n):
+
+
     for i in range(n):
         if occupied[i]==0:
             print(l[i], end=' ')
