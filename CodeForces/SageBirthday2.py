8c9d314cd67ddc3474eb052b2186ca5dd6f6480b
PratikGarai <pratikgarai0208@gmail.com>
2020-09-23 23:24:31 +0530

    Removed redundant code

--- CodeForces/SageBirthday2.py
+++ CodeForces/SageBirthday2.py
@@ -6,26 +6,6 @@ def main():
 
     occupied = [0 for i in range(n)]
     
-    l_pointer = 0
-    h_pointer = l_pointer + 1
-
-    while(h_pointer<n-1 and l_pointer<n-2):
-
-        occupied[l_pointer] = 1
-
-        while(l[h_pointer]<=l[l_pointer] and h_pointer<n-1):
-            h_pointer += 1
-
-        print(l[h_pointer],l[l_pointer],l[h_pointer+1], sep=' ', end=' ')
-        occupied[h_pointer] = 1
-        occupied[h_pointer+1] = 1
-        h_pointer += 1
-
-        while(l[l_pointer]!=0 and l_pointer<n-2):
-            l_pointer += 1
-        if l_pointer>=h_pointer:
-            h_pointer = l_pointer+1
-
     for i in range(n):
         if occupied[i]==0:
             print(l[i], end=' ')
