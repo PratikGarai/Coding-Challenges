5caa96c0530d849f0fc995e1a1ef06265e732921
PratikGarai <pratikgarai0208@gmail.com>
2020-09-29 20:08:12 +0530

    works for max case, still figuring out min

--- CodeForces/RockPaperScissors.py
+++ CodeForces/RockPaperScissors.py
@@ -1,9 +1,37 @@
+def min_wins(a,b):
+    d1 = min(a[0],b[0])
+    d2 = min(a[1],b[1])
+    d3 = min(a[2],b[2])
+
+    a[0] -= d1
+    b[0] -= d1
+    a[1] -= d2
+    b[1] -= d2
+    a[2] -= d3
+    b[2] -= d3
+
+    l1 = min(a[0],b[2])
+    l2 = min(a[1],b[0])
+    l3 = min(a[2],b[1])
+
+    a[0] -= l1
+    b[2] -= l1
+    a[1] -= l2
+    b[0] -= l2
+    a[2] -= l3
+    b[1] -= l3
+    
+    return sum(a)
+
+def max_wins(a, b):
+    return( min(a[0],b[1])+min(a[1],b[2])+min(a[2],b[0]))
+
 def main():
     n= int(input())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    print(min_wins(a,b))
+    print(min_wins(a.copy(),b.copy()), end = ' ')
     print(max_wins(a,b))
 
 if __name__=='__main__':
