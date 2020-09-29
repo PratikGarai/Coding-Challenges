36b0418ad68697dbf96078e132de98afb2a1481f
PratikGarai <pratikgarai0208@gmail.com>
2020-09-29 19:39:04 +0530

    part B done

--- CodeForces/SymmetricMatrix.py
+++ CodeForces/SymmetricMatrix.py
@@ -1,6 +1,21 @@
 def process():
     n, m = list(map(int, input().split()))
 
+    flag = 0
+    for i in range(n):
+        tile = []
+        tile.append(list(input().split()))
+        tile.append(list(input().split()))
+        if (tile[0][1]==tile[1][0]):
+            flag = 1
+
+    if m%2 :
+         print('NO')
+    elif flag :
+        print('YES')
+    else :
+        print('NO')
+
 def main():
     for i in range(int(input())):
         process()
