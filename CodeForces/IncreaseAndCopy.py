28190f589aa58faea9132bb48707e93605533421
PratikGarai <pratikgarai0208@gmail.com>
2020-09-30 09:50:49 +0530

    C done

--- CodeForces/IncreaseAndCopy.py
+++ CodeForces/IncreaseAndCopy.py
@@ -2,19 +2,28 @@ def process():
     n = int(input())
 
     if n==1:
+        print(0)
         return 0 
-    
-    moves = 0
+    moves = 100000000
+    p = int(n**0.5)+1
+    for i in range(p-10, p+10):
+        if i>0 and i<=n:
+            m =(i-2)+n/i
+            if(m%1!=0):
+                m += 1
+            moves = min(moves, m)
 
-    return moves
+    # return int(moves)
+    print(int(moves))
 
 def main():
-    res = []
+    # res = []
     for i in range(int(input())):
-        res.append(process())
+        # res.append(process())
+        process()
 
-    for i in res :
-        print(i)
+    # for i in res :
+    #     print(i)
 
 if __name__=='__main__':
     main()
