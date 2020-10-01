e70232673daab9ea9f4c2b727164b9a2b9e94186
PratikGarai <pratikgarai0208@gmail.com>
2020-10-01 22:48:02 +0530

    Added some code

---
+++ CodeForces/TwoArrays.py
@@ -0,0 +1,33 @@
+def process():
+    n, T = list(int, input().split())
+    l = list(map(int, input().split()))
+
+    if T%2 :
+        T = T//2
+        for i in l:
+            if T<=i:
+                print(0, end=' ')
+            else :
+                print(1, end=' ')
+    else:
+        h = T//2
+        flag = 0
+        for i in l:
+            if h<i:
+                print(0, end=' ')
+            elif h>i:
+                print(1, end=' ')
+            else :
+                print(flag, end=' ')
+                if flag==0:
+                    flag = 1
+                else:
+                    flag = 0
+
+
+def main():
+    for i in range(int(input())):
+        process()
+
+if __name__='__main__':
+    main()
