6796d7be5dff8234bd79e39e1224f28e2ce5a9c0
PratikGarai <pratikgarai0208@gmail.com>
2020-10-19 09:41:51 +0530

    input implemented

--- Google_Leaning/Basic_Programming/1_Subsequence_Problem.cpp
+++ Google_Leaning/Basic_Programming/1_Subsequence_Problem.cpp
@@ -4,8 +4,29 @@
 #include<iostream>
 using namespace std;
 
+string S;
+string* D;
+
 int main()
 {
+	int l,l_s,l_w,i;
+	cout<<"Enter the length of 'S' : ";
+	cin>>l_s;
+	cout<<"Enter 'S' : ";
+	cin>>S;
+	cout<<"Enter the number of words : ";
+	cin>>l;
+	D = new string[l];
+	for(i=0;i<l;i++)
+	{
+		cout<<"Enter the length of "<<i+1<<"th word : ";
+		cin>>l_w;
+		cout<<"Enter the word : ";
+		cin>>D[i];
+	}
+	cout<<"\nMain word : "<<S<<endl;
+	for(i=0;i<l;i++)
+		cout<<i+1<<"th word : "<<D[i]<<endl;
 
 	return 0;
 }
