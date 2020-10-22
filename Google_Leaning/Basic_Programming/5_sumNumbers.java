76ab915dbb12d826aa4e7875aaabe51b62cc5dac
PratikGarai <pratikgarai0208@gmail.com>
2020-10-22 17:04:31 +0530

    Added the core code

---
+++ Google_Leaning/Basic_Programming/5_sumNumbers.java
@@ -0,0 +1,25 @@
+
+public int sumNumbers(String str) 
+{
+  int g_sum = 0, l_sum=0, diff;
+  boolean is_num = false;
+  for(int i=0;i<str.length();i++)
+  {
+    diff =  str.charAt(i)-'0';
+    if(diff>=0&&diff<=9)
+    {
+      l_sum = l_sum*10+diff;
+      if(!is_num)
+        is_num = true;
+    }
+    else if(is_num)
+    {
+      is_num = false;
+      g_sum += l_sum;
+      l_sum = 0;
+    }
+  }
+  if(is_num)
+    g_sum += l_sum;
+  return g_sum;
+}
