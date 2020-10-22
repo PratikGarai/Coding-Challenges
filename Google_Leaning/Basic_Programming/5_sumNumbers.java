796977479b323fb70358aad76aaf357cbe36805f
PratikGarai <pratikgarai0208@gmail.com>
2020-10-22 17:09:37 +0530

    Completed the full code

--- Google_Leaning/Basic_Programming/5_sumNumbers.java
+++ Google_Leaning/Basic_Programming/5_sumNumbers.java
@@ -1,25 +1,38 @@
+import java.util.Scanner;
 
-public int sumNumbers(String str) 
+class SumNumbers
 {
-  int g_sum = 0, l_sum=0, diff;
-  boolean is_num = false;
-  for(int i=0;i<str.length();i++)
-  {
-    diff =  str.charAt(i)-'0';
-    if(diff>=0&&diff<=9)
-    {
-      l_sum = l_sum*10+diff;
-      if(!is_num)
-        is_num = true;
-    }
-    else if(is_num)
-    {
-      is_num = false;
-      g_sum += l_sum;
-      l_sum = 0;
-    }
-  }
-  if(is_num)
-    g_sum += l_sum;
-  return g_sum;
+	public int sumNumbers(String str) 
+	{
+  		int g_sum = 0, l_sum=0, diff;
+  		boolean is_num = false;
+  		for(int i=0;i<str.length();i++)
+  		{
+    			diff =  str.charAt(i)-'0';
+    			if(diff>=0&&diff<=9)
+    			{
+      				l_sum = l_sum*10+diff;
+      				if(!is_num)
+        				is_num = true;
+    			}
+    			else if(is_num)
+    			{
+      				is_num = false;
+      				g_sum += l_sum;
+      				l_sum = 0;
+    			}
+  		}
+  		if(is_num)
+    			g_sum += l_sum;
+  		return g_sum;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		SumNumbers ob = new SumNumbers();
+		System.out.print("Enter the string : ");
+		String a = in.nextLine();
+		System.out.println("The sum : "+ob.sumNumbers(a));
+	}	
 }
