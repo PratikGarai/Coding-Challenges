490190ace207368f7124f8423de51db46de6c69c
PratikGarai <pratikgarai0208@gmail.com>
2020-10-26 21:52:48 +0530

    Added the code

---
+++ Google_Leaning/Basic_Programming/11_word0.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+class Word_Zero
+{
+	public Map<String, Integer> word0(String[] strings) 
+	{
+  		Map<String , Integer> a = new HashMap<String, Integer>();
+  		for(String s : strings)
+    			a.put(s,0);
+  		return a;
+	}
+
+	public static void main(String[] args)
+	{
+		Word_Zero ob = new Word_Zero();
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of elements : ");
+		int n = in.nextInt();
+		String a[] = new String[n];
+		for(int i=0;i<n;i++)
+		{
+			System.out.print("Enter the "+(i+1)+"th string : ");
+			a[i] = in.next();
+		}
+
+		Map<String, Integer> map = ob.word0(a);
+		System.out.println("-------------------------\nThe values :\n"); 
+		for (Map.Entry<String,Integer> entry : map.entrySet()) 
+		{
+    			String key = entry.getKey();
+    			int value = entry.getValue();
+			System.out.println(key+"  -->  "+value);
+		}
+	}
+}
