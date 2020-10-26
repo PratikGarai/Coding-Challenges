815032f2f40921e4bdc988555b34df3d8f1fc1eb
PratikGarai <pratikgarai0208@gmail.com>
2020-10-26 21:13:33 +0530

    Added full code

--- Google_Leaning/Basic_Programming/9_simpleInterpreter.java
+++ Google_Leaning/Basic_Programming/9_simpleInterpreter.java
@@ -1,21 +1,50 @@
-public int interpret(int value, String[] commands, int[] args) {
-  int val = value, l = commands.length;
-  for(int i=0;i<l;i++)
-  {
-    switch(commands[i].charAt(0))
-    {
-      case '+':
-        val += args[i];
-        break;
-      case '*':
-        val *= args[i];
-        break;
-      case '-':
-        val -= args[i];
-        break;
-      default :
-        return -1;
-    }
-  }
-  return val;
+import java.util.Scanner;
+
+class SimpleInterpreter
+{
+	public int interpret(int value, String[] commands, int[] args) 
+	{
+  		int val = value, l = commands.length;
+  		for(int i=0;i<l;i++)
+  		{
+    			switch(commands[i].charAt(0))
+    			{
+      				case '+':
+        				val += args[i];
+        				break;
+      				case '*':
+        				val *= args[i];
+        				break;
+      				case '-':
+        				val -= args[i];
+        				break;
+      				default :
+        				return -1;
+    			}
+  		}
+  		return val;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		SimpleInterpreter ob = new SimpleInterpreter();
+		System.out.print("Enter the initial value : ");
+		int value = in.nextInt();
+		System.out.print("Enter the number of operators : ");
+		int t = in.nextInt();
+		
+		String commands[] = new String[t];
+		int arg[] = new int[t];
+		for(int i=0;i<t;i++)
+		{
+			System.out.print("Enter the "+(i+1)+"th command : ");
+			commands[i] = in.next();
+			System.out.print("Enter the "+(i+1)+"th argument : ");
+			arg[i] = in.nextInt();
+		}
+
+		System.out.println("The result : "+ob.interpret(value, commands, arg));
+
+	}
 }
