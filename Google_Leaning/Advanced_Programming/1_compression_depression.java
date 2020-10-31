23fa96b1e972b3ecb2988e36d73809598a69abcb
PratikGarai <pratikgarai0208@gmail.com>
2020-10-31 11:10:45 +0530

    Done..............

--- Google_Leaning/Advanced_Programming/1_compression_depression.java
+++ Google_Leaning/Advanced_Programming/1_compression_depression.java
@@ -12,39 +12,51 @@ class Compression_Depression
 	{
 		if(f>l)
 			return "";
-		if(Character.isDigit(a.charAt(f)))
+		int m_ind = f;
+		String s = "";
+		while(m_ind <= l)
 		{
-			String s = "";
-			int ind = f;
-			int n=0;
-			while(Character.isDigit(a.charAt(ind)))
+			if(Character.isDigit(a.charAt(m_ind)))
 			{
-				n = n*10 + a.charAt(ind) -'0';
+				int ind = m_ind;
+				int n=0;
+				while(Character.isDigit(a.charAt(ind)))
+				{
+					n = n*10 + a.charAt(ind) -'0';
+					ind++;
+				}
 				ind++;
+				int bractes = 1, begin = ind;
+				while(bractes!=0)
+				{
+					if(a.charAt(ind)=='[')
+						bractes++;
+					if(a.charAt(ind)==']')
+						bractes--;
+					ind++;
+				}
+				int end = ind-2;
+				m_ind = ind;
+				String st = decompress(begin, end);
+				for(int i=0;i<n;i++)
+					s += st;
 			}
-			ind++;
-			int bractes = 1, begin = ind;
-			while(bractes!=0)
+			else
 			{
-				if(a.charAt(ind)=='[')
-					bractes++;
-				if(a.charAt(ind)==']')
-					bractes--;
-				ind++;
+				int ind = m_ind;
+				int begin = m_ind;
+				while(ind<=l)
+				{
+					char c = a.charAt(ind);
+					if( (c>=65 && c<=90)||(c>=97 && c<=122)) 
+						ind++;
+				}
+				int end = ind-1;
+				m_ind = ind;
+				s += a.substring(begin, end+1);
 			}
-			int end = ind-2;
-			System.out.println(begin+" "+end);
-			String st = decompress(begin, end);
-			System.out.println(st);
-			for(int i=0;i<n;i++)
-				s += st;
-			return s;
 		}
-		else
-		{
-			return a.substring(f,l+1);
-		}
-
+		return s;
 	}
 
 	public static void main(String[] args)
