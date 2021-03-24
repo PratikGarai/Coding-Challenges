1779d042cc8e4a1b067af75410c888e576e5490f
PratikGarai <pratikgarai0208@gmail.com>
2021-03-24 07:58:19 +0530

    Added TEST

---
+++ SPOJ/TEST.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	   
+	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	int x;
+	while(cin >> x)
+		if(x==42)
+			break;
+		else 
+			cout << x << "\n";
+
+	return 0;
+}
