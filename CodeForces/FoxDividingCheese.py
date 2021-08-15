0ef24c7f369341e56404a1d024fd3f066a69cd6f
PratikGarai <pratikgarai0208@gmail.com>
2021-08-15 23:35:48 +0530

    Corrected FoxDividingCheese

--- CodeForces/FoxDividingCheese.py
+++ CodeForces/FoxDividingCheese.py
@@ -8,38 +8,52 @@ def process(a, b, step):
         if(a%5==0):
             val = process(a//5, b, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        elif(a%3==0):
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
+        if(a%3==0):
             val = process(a//3, b, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        elif(a%2==0):
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
+        if(a%2==0):
             val = process(a//2, b, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        else :
-            return -1
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
     else:
         if(b%5==0):
             val = process(a, b//5, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        elif(b%3==0):
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
+        if(b%3==0):
             val = process(a, b//3, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        elif(b%2==0):
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
+        if(b%2==0):
             val = process(a, b//2, step+1)
             if val!=-1:
-                steps = min(steps, val)
-                changed = True
-        else :
-            return -1
+                if changed : 
+                    steps = min(steps, val)
+                else:
+                    steps = val
+                    changed = True
     if not changed:
         return -1
     else :
