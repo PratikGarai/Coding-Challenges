b2a8dfa40888b3387011cb8fbbc5cdc483f86342
PratikGarai <pratikgarai0208@gmail.com>
2022-02-14 12:25:18 +0530

    Added Searching a number

---
+++ GFG/SearchingANumber.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+
+	
+	def search(self,arr, n, k): 
+    	# code here
+    	for i in range(n):
+    	    if arr[i]==k : 
+    	        return i+1
+    	
+    	return -1
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n, k=map(int, input().strip().split())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.search(arr, n, k)
+        print(ans)
+        tc=tc-1
+# } Driver Code Ends
