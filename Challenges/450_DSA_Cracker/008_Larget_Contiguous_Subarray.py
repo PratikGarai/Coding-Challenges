2ad994c233bb3769b91468903571d4b398da5338
PratikGarai <pratikgarai0208@gmail.com>
2023-04-27 23:28:51 +0530

    008 of 450 py

---
+++ Challenges/450_DSA_Cracker/008_Larget_Contiguous_Subarray.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr,N):
+        
+        maxSum = -10e7
+        hereSum = 0
+        for i in range(N) :
+            if hereSum < 0 :
+                hereSum =  0
+            hereSum += arr[i]
+            maxSum = max(maxSum, hereSum)
+        return maxSum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            
+            print(ob.maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
