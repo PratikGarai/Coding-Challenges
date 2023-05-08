f78ceae6e700315023a98db876b57e9cec4321d4
PratikGarai <pratikgarai0208@gmail.com>
2023-05-08 22:27:26 +0530

    011 of 450 py

---
+++ Challenges/450_DSA_Cracker/011_Find_Duplicate_In_N1_Integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        s = set()
+        for i in nums : 
+            if i in s : 
+                return i
+            s.add(i)
