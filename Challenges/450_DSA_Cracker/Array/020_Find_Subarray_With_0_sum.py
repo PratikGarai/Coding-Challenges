a2fe186dc843d2cacf798baa30b198a62800755e
PratikGarai <pratikgarai0208@gmail.com>
2023-05-23 11:20:30 +0530

    Added 20 of 450 Array

---
+++ Challenges/450_DSA_Cracker/Array/020_Find_Subarray_With_0_sum.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        sums = set()
+        s = 0
+        sums.add(0)
+        for i in arr : 
+            s += i
+            if s in sums : 
+                return True
+            sums.add(s)
+        return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
