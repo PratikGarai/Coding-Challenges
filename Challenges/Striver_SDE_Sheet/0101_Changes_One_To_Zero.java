76715ad8de18aa0ee6459a27c3d63d892b68483e
PratikGarai <pratikgarai0208@gmail.com>
2024-01-06 13:52:26 +0530

    Added SSS 0101 sol 2

--- Challenges/Striver_SDE_Sheet/0101_Changes_One_To_Zero.java
+++ Challenges/Striver_SDE_Sheet/0101_Changes_One_To_Zero.java
@@ -42,3 +42,36 @@ class Solution {
         }
     }
 }
+
+
+/*
+* Solution 2
+* Store which rows and columns to zero out in 2 different single dimnsion arrays
+*/
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] r_zero = new int[rows];
+        int[] c_zero = new int[cols];
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(matrix[i][j]==0) {
+                    r_zero[i] = 1;
+                    c_zero[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(r_zero[i] == 1 || c_zero[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
