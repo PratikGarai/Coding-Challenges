d353adcd0b9380b23d5b5d043476358c76cc9fcc
PratikGarai <pratikgarai0208@gmail.com>
2024-01-08 21:38:08 +0530

    Added SSS 0106

---
+++ Challenges/Striver_SDE_Sheet/0106_Buy_And_Sell.java
@@ -0,0 +1,14 @@
+/*
+Since we can buy or sell on any day, we scan and store the lowest priceand compare it to all subsequent prices.
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max_profit = 0;
+        for(int i:prices) {
+            max_profit = Math.max(max_profit, i-min);
+            min = Math.min(min, i);
+        }
+        return max_profit;
+    }
+}
