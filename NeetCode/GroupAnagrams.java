8e316922865f777989186817270374cff4492d6f
PratikGarai <pratikgarai0208@gmail.com>
2024-07-13 15:59:00 +0000

    Added group anagrams approach 3

--- NeetCode/GroupAnagrams.java
+++ NeetCode/GroupAnagrams.java
@@ -75,3 +75,42 @@ class Solution {
         return result;
     }
 }
+
+/**
+    Create a key for each string by sorting it.
+    Space : O(n)
+    Complexity : O(n * 26 * k).
+    Faster than solution 2 because of the use of StringBuilder.
+ */
+class Solution {
+    public static String createKey(String s) {
+        int[] counter = new int[26];
+        for(int i =0; i<s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int count : counter) {
+            sb.append('#').append(count);
+        }
+        return sb.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> h = new HashMap<String, List<String>>();
+        String generatedKey = null;
+        for(String s : strs) {
+            generatedKey = createKey(s);
+            if(h.containsKey(generatedKey)) {
+                h.get(generatedKey).add(s);
+            } else {
+                h.put(generatedKey, new ArrayList<String>());
+                h.get(generatedKey).add(s);
+            }
+        }    
+        List<List<String>> result = new ArrayList<List<String>>();
+        for(String key : h.keySet()) {
+            result.add(h.get(key));
+        }
+        return result;
+    }
+}
