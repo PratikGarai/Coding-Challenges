23c69be9f7707bf9e8c59bc880858588b049723d
PratikGarai <pratikgarai0208@gmail.com>
2024-07-13 15:51:12 +0000

    Added group anagrams approach 2

--- NeetCode/GroupAnagrams.java
+++ NeetCode/GroupAnagrams.java
@@ -29,3 +29,49 @@ class Solution {
         return result;
     }
 }
+
+/**
+    Create a key for each string by sorting it.
+    Space : O(n)
+    Complexity : O(n * (26 + k))
+
+    Slower than previous solution. Time difference is caused due to log(k) vs 26.
+ */
+class Solution {
+    public static String createKey(String s) {
+        int[] counter = new int[26];
+        for(int i =0; i<s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+        }
+        String k = "";
+        for(int i=0; i<26; i++){
+            if(counter[i]<10) {
+                k += "00"+Integer.toString(counter[i]);
+            } else if(counter[i]<100) {
+                k += "0"+Integer.toString(counter[i]);
+            } else {
+                k += "100";
+            }
+        }
+        return k;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> h = new HashMap<String, List<String>>();
+        String generatedKey = null;
+        for(String s : strs) {
+            generatedKey = createKey(s);
+            if(h.containsKey(generatedKey)) {
+                h.get(generatedKey).add(s);
+            } else {
+                h.put(generatedKey, new ArrayList<String>());
+                h.get(generatedKey).add(s);
+            }
+        }    
+        List<List<String>> result = new ArrayList<List<String>>();
+        for(String key : h.keySet()) {
+            result.add(h.get(key));
+        }
+        return result;
+    }
+}
