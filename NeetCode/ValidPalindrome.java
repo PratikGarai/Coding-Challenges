346dfe3306ba5504c37c239e9ecd4ac8254e8ade
PratikGarai <pratikgarai0208@gmail.com>
2024-07-18 10:50:11 +0000

    Added valid palindrome

---
+++ NeetCode/ValidPalindrome.java
@@ -0,0 +1,30 @@
+/*
+ * Two pointers, left and right.
+ * Comapre ignorecae for valid alphanumeric.
+ */
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l=0, r=s.length()-1;
+        char cl, cr;
+        while(l<r) {
+            cl = s.charAt(l);
+            if(!(Character.isDigit(cl) || Character.isAlphabetic(cl))) {
+                l++;
+                continue;
+            }
+            cr = s.charAt(r);
+            if(!(Character.isDigit(cr) || Character.isAlphabetic(cr))) {
+                r--;
+                continue;
+            }
+
+            if(Character.toLowerCase(cl) != Character.toLowerCase(cr)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
